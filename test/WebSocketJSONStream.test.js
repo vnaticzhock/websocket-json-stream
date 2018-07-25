a6d662ac2a6f9fe0db320c1b8220edee80b3f3d3
Nate Smith <nate@nateps.com>
2018-07-24 22:50:53 -0700

    fix eslint issues

--- test/WebSocketJSONStream.test.js
+++ test/WebSocketJSONStream.test.js
@@ -2,6 +2,7 @@ const assert = require('chai').assert
 const http = require('http')
 const WebSocket = require('ws')
 const WebSocketJSONStream = require('..')
+const testCloseStatus = require('./close-status')
 
 const handler = (done, code) => (...args) => {
     try {
@@ -229,96 +230,5 @@ describe('WebSocketJSONStream', function () {
         clientWebSocket.on('open', () => clientWebSocket.close())
     })
 
-    it('should emit CloseEvent with code 1000 and reason "stream end" on clientStream.end()', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.code, 1000)
-            assert.equal(event.reason, 'stream end')
-            done()
-        }
-        this.clientStream.end()
-    })
-    it('should emit CloseEvent with code 1000 and reason "stream end" on serverStream.end()', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.code, 1000)
-            assert.equal(event.reason, 'stream end')
-            done()
-        }
-        this.serverStream.end()
-    })
-    it('should emit CloseEvent with code 1005 and reason "" on clientStream.destroy()', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.code, 1005)
-            assert.equal(event.reason, '')
-            done()
-        }
-        this.clientStream.destroy()
-    })
-    it('should emit CloseEvent with code 1005 and reason "" on serverStream.destroy()', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.code, 1005)
-            assert.equal(event.reason, '')
-            done()
-        }
-        this.serverStream.destroy()
-    })
-    it('should emit CloseEvent with code 1011 and reason "stream error" on clientStream.destroy(error)', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.code, 1011)
-            assert.equal(event.reason, 'stream error')
-            done()
-        }
-        const error = new Error('Test error')
-        this.clientStream.on('error', e => {})
-        this.clientStream.destroy(error)
-    })
-    it('should emit CloseEvent with code 1011 and reason "stream error" on serverStream.destroy(error)', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.code, 1011)
-            assert.equal(event.reason, 'stream error')
-            done()
-        }
-        const error = new Error('Test error')
-        this.serverStream.on('error', e => {})
-        this.serverStream.destroy(error)
-    })
-    it('should emit CloseEvent with code from error.closeCode on clientStream.destroy(error)', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.code, 4000)
-            done()
-        }
-        const error = new Error('Test error')
-        error.closeCode = 4000
-        this.clientStream.on('error', e => {})
-        this.clientStream.destroy(error)
-    })
-    it('should emit CloseEvent with code from error.closeCode on serverStream.destroy(error)', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.code, 4000)
-            done()
-        }
-        const error = new Error('Test error')
-        error.closeCode = 4000
-        this.serverStream.on('error', e => {})
-        this.serverStream.destroy(error)
-    })
-    it('should emit CloseEvent with reason from error.closeReason on clientStream.destroy(error)', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.reason, 'custom reason')
-            done()
-        }
-        const error = new Error('Test error')
-        error.closeReason = 'custom reason'
-        this.clientStream.on('error', e => {})
-        this.clientStream.destroy(error)
-    })
-    it('should emit CloseEvent with reason from error.closeReason on serverStream.destroy(error)', function (done) {
-        this.clientWebSocket.onclose = (event) => {
-            assert.equal(event.reason, 'custom reason')
-            done()
-        }
-        const error = new Error('Test error')
-        error.closeReason = 'custom reason'
-        this.serverStream.on('error', e => {})
-        this.serverStream.destroy(error)
-    })
+    testCloseStatus()
 })
